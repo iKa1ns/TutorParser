da93b0dd4a09db6a569d672dfc8d6f1026ce371d
ika1n <ika1n@mail.ru>
2024-10-18 19:47:18 +0300

    Добавление файла логики

---
+++ logic.py
@@ -0,0 +1,16 @@
+def get_html(url: str) -> str:
+    """
+    Функция для отправки запроса
+    :param url:
+    :return:
+    """
+    pass
+
+
+def get_tasks(html: str) -> list[str]:
+    """
+    Функция для вычленения названий задач
+    :param html:
+    :return:
+    """
+    pass
